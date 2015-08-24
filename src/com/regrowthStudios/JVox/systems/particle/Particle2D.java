10b087f17ff9443b54459aa1ca1137e8d2c5d027
barnold1953 <brb555@utk.edu>
2015-08-23 20:17:42 -0400

    Updated to new particle emitter system

--- src/com/regrowthStudios/JVox/systems/particle/Particle2D.java
+++ src/com/regrowthStudios/JVox/systems/particle/Particle2D.java
@@ -5,36 +5,23 @@ import org.newdawn.slick.opengl.Texture;
 import com.regrowthStudios.JVox.graphics.SpriteBatch;
 
 public class Particle2D {
+    
     public float x, y;
-    private float vx, vy;
-    private float sx, sy;
+    protected float vx, vy;
+    protected float sx, sy;
     public int maxLife, currentLife;
-    private Texture texture;
-    private byte[] color = new byte[4];
 
-    public Particle2D() {
-    }
-
-    public void init(Texture t, float x, float y, float vx, float vy, float sx, float sy) {
+    public void init(float x, float y, float vx, float vy, float sx, float sy) {
         this.x = x;
         this.y = y;
         this.vx = vx;
         this.vy = vy;
         this.sx = sx;
         this.sy = sy;
-        texture = t;
-        color[0] = (byte) 255;
-        color[1] = (byte) 255;
-        color[2] = (byte) 255;
-        color[3] = (byte) 255;
     }
 
     public void update() {
         x += vx;
         y += vy;
-    }
-
-    public void render(SpriteBatch batch) {
-        batch.draw(texture.getTextureID(), x + sx * 0.5f, y + sy * 0.5f, sx, sy, 0.0f, 0.0f, texture.getWidth(), texture.getHeight(), 0.0f, color);
     }
 }
--- src/com/regrowthStudios/JVox/systems/particle/ParticleEmitter.java
+++ src/com/regrowthStudios/JVox/systems/particle/ParticleEmitter.java
@@ -13,36 +13,57 @@ import com.regrowthStudios.JVox.math.Vector;
 import com.regrowthStudios.JVox.utils.EventUtils.MouseEvents;
 
 public class ParticleEmitter {
+    static Random random = new Random();
+    public float x, y;
+    public int particlesPerSpawn = 1;
+    public int framesUntilSpawn = 1;
+    public int maxParticleLife = 500;
+    public int minParticleLife = 250;
+    public float particleWidth = 5.0f;
+    public float particleHeight = 5.0f;
+    public Vector direction = new Vector();
+    public float spawnAngle = (float)Math.PI;
+    public byte color[] = new byte[4];
+    private int frame = 0;
     private ArrayList<Particle2D> particles = new ArrayList<Particle2D>();
-    private Random rand = null;
     private Texture particleTexture = null;
-    Random random;
 
-    public void init() {
-        this.rand = new Random();
-
-        try {
-            particleTexture = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("data/textures/spark.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        random = new Random();
+    public void init(Texture texture, float x, float y, float dirX, float dirY) {
+        particleTexture = texture;
+        color[0] = (byte)255;
+        color[1] = (byte)255;
+        color[2] = (byte)255;
+        color[3] = (byte)255;
+        this.x = x;
+        this.y = y;
+        direction.x = dirX;
+        direction.y = dirY;
+        direction.normalizeLocal();
+    }
+    
+    public void setColor(byte r, byte g, byte b, byte a) {
+        color[0] = r;
+        color[1] = g;
+        color[2] = b;
+        color[3] = a;
     }
 
-    public void update(Vector mousePos) {
-      
-        if (MouseEvents.buttonDown(0)) {
-            for (int i = 0; i < 1000; i++) {
-                Particle2D tmp = new Particle2D();
-                tmp.init(particleTexture, (float)mousePos.x, (float)mousePos.y,
-                        ((random.nextInt(Integer.MAX_VALUE)) / (float)(Integer.MAX_VALUE) * 2.0f - 1.0f),
-                        ((random.nextInt(Integer.MAX_VALUE)) / (float)(Integer.MAX_VALUE) * 2.0f - 1.0f), 
-                        5.0f, 5.0f);
-                tmp.maxLife = rand.nextInt((500 - 250) + 1) + 250;
+    public void update() {
+        frame++;
+        if (frame >= framesUntilSpawn) {
+            for (int i = 0; i < particlesPerSpawn; i++) {
+                Particle2D p = new Particle2D();
+                
+                float angle = ((random.nextInt(Integer.MAX_VALUE)) / (float)(Integer.MAX_VALUE) * 2.0f - 1.0f) * spawnAngle;
+                float dx = (float)direction.x * (float) Math.cos(angle) - (float)direction.y * (float) Math.sin(angle);
+                float dy = (float)direction.x * (float) Math.sin(angle) + (float)direction.y * (float) Math.cos(angle);
+                
+                p.init((float)x, (float)y, dx, dy, particleWidth, particleHeight);
+                p.maxLife = random.nextInt((maxParticleLife - minParticleLife) + 1) + minParticleLife;
     
-                particles.add(tmp);
+                particles.add(p);
             }
+            frame = 0;
         }
 
         if (particles.size() > 0) {
@@ -60,35 +81,15 @@ public class ParticleEmitter {
                     }
                 }
             }
-            System.out.println(particles.size());
         } else {
             particles.trimToSize();
         }
 
-        /*
-         * System.out.println(this.particles.size());
-         * 
-         * for (int i = 0; i < particles.size(); i++) { Particle2D tmp =
-         * particles.get(i);
-         * 
-         * if (tmp != null) { //tmp.getPosition().add(rand.nextInt((1 - -1) + 1)
-         * + -1, rand.nextInt((1 - -1) + 1) + -1);
-         * 
-         * if (tmp.currentLife > tmp.maxLife) { particles.remove(i);
-         * particles.trimToSize(); } else { tmp.update(); tmp.currentLife++; } }
-         * }
-         */
     }
 
     public void render(SpriteBatch batch) {
-        for (int i = 0; i < particles.size(); i++) {
-            Particle2D tmp = particles.get(i);
-
-            if (tmp != null) {
-                // if ((rand.nextInt((10 - 0) + 1) + 0) == 1) {
-                tmp.render(batch);
-                // }
-            }
+        for (Particle2D p : particles) {
+            batch.draw(particleTexture.getTextureID(), p.x + p.sx * 0.5f, p.y + p.sy * 0.5f, p.sx, p.sy, 0.0f, 0.0f, particleTexture.getWidth(), particleTexture.getHeight(), 0.0f, color);
         }
     }
 }
